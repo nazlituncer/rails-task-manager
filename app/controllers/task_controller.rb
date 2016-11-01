53a77a832a411500d8391120d650378415c99dc8
Nazli Tuncer <nazlituncer@gmail.com>
2016-11-01 15:16:49 +0100

    Task manager

--- app/controllers/task_controller.rb
+++
@@ -1,4 +0,0 @@
-class TaskManagerController < ApplicationController
-
-end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101104245) do
+ActiveRecord::Schema.define(version: 20161101113337) do
 
-  create_table "task_managers", force: :cascade do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "content"
-    t.boolean  "completed"
+    t.string   "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
